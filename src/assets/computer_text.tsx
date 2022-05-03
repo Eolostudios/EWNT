8c164050a7ba71cead42321d739a6b1d95022737
EoloStudios <Eolostudios@posteo.net>
2022-05-03 15:27:00 +0200

    hope

---
+++ src/assets/computer_text.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="computer_text" tilewidth="32" tileheight="32" tilecount="3430" columns="49">
+ <image source="computer_text.png" width="1587" height="2245"/>
+</tileset>
